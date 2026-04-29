88171ba29d4b391aea2fd9bf0c04dbb8f9d42731
Boris <borismock972@gmail.com>
2026-04-29 11:34:36 +0200

    commentaire preprocess

--- plan_processing/pipeline/preprocess.py
+++ plan_processing/pipeline/preprocess.py
@@ -1,11 +1,7 @@
 """
-Boris MOCK
-boris.mock@isen-ouest.yncrea.fr
--------------------------------
-
-L'objectif est de prétraiter l'image chargée
-Voici la 1er étape du processus complet
+The first step of the process
 """
+
 
 import cv2 as cv
 import numpy as np
@@ -13,19 +9,19 @@ import numpy as np
 
 def preprocess(image: np.ndarray):
     """
-    Pré prépare l'image pour la binarisation qui suit juste après
-    Dans un premier temps on va avoir une conversion en niveaux de gris
-    puis une réduction de bruit  avec un filtre gaussien pas tres utile car en général
-    les vrais plans sont propre mais si on a un autre format que svg cela peut etre utile
+    Pre-preparing the image for binarization, which follows immediately.
+    First, we'll perform a grayscale conversion.
+    Then, noise reduction with a Gaussian filter, which isn't very useful because, generally,
+    the actual images are clean, but if you have a format other than SVG, it can be helpful.
 
-    :param image: RGB, retourné par loader.py
-    :return: image en niveau de gris débruitée
+    :param image: RGB, return by loader.py
+    :return: grayscale image denoised
     """
 
-    gray = cv.cvtColor(image, cv.COLOR_RGB2GRAY)
+    gray = cv.cvtColor(image, cv.COLOR_RGB2GRAY)    #conversion image RGB to gray
 
-    # filtre gaussien fait la moyenne des pixel autour
-    denoised = cv.GaussianBlur(gray, ksize=(3,3), sigmaX=0) # ksize = zone de pixel autour d'un pixel (impair)
-                                                            # sigmax = intensité du lissage, cad les pixels loins ont plus de poids
+    # gaussian filter do the mean of pixel arround
+    denoised = cv.GaussianBlur(gray, ksize=(3,3), sigmaX=0) # ksize =pixel zone arround one pixel (odd)
+                                                            # sigmax = intensity smoothing, pixels far away are more heavy
     return denoised
 
